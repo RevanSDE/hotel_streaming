d7000135ae6c6f92d727c5e2871f578b7b57e9a5
RevanSource <sitnikov_itip@mail.ru>
2020-02-06 11:00:53 +0300

    add kafka producer

--- gatewayservice/src/main/java/gatewayservice/service/KafkaService.java
+++ gatewayservice/src/main/java/gatewayservice/service/KafkaService.java
@@ -2,12 +2,23 @@ package gatewayservice.service;
 
 import gatewayservice.model.Room;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class KafkaService {
 
+    @Value("${kafka.topic}")
+    private String topic;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     public void sendToKafka(Room room) {
+        //TODO transform to json or avro
+        kafkaTemplate.send(topic, room.toString());
     }
 }
